88b76c3a2b85ccd0761f3630bf41144be9f4d78f
夜雨 <yeyu54876@gmail.com>
2026-02-11 22:45:45 +0800

    添加友链kiyukie'Blog

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -11,6 +11,15 @@ export const friendsPageConfig: FriendsPageConfig = {
 // 友链配置
 export const friendsConfig: FriendLink[] = [
 	{
+		title: "Sky Blog",
+		imgurl: "https://avatars.githubusercontent.com/u/165529493",
+		desc: "华风夏韵 洛水天依！",
+		siteurl: "https://blog.sky1314.dpdns.org/",
+		tags: ["Blog"],
+		weight: 10,
+		enabled: false, 
+	},
+	{
 		title: "夏夜流萤",
 		imgurl: "https://q1.qlogo.cn/g?b=qq&nk=7618557&s=640",
 		desc: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
@@ -20,28 +29,18 @@ export const friendsConfig: FriendLink[] = [
 		enabled: true, // 是否启用
 	},
 	{
-		title: "Firefly Docs",
-		imgurl: "https://docs-firefly.cuteleaf.cn/logo.png",
-		desc: "Firefly主题模板文档",
-		siteurl: "https://docs-firefly.cuteleaf.cn",
-		tags: ["Docs"],
-		weight: 8,
-		enabled: true, 
-	},
-	{
-		title: "Sky Blog",
-		imgurl: "https://avatars.githubusercontent.com/u/165529493?v=4&size=64",
-		desc: "华风夏韵 洛水天依！",
-		siteurl: "https://blog.sky1314.dpdns.org/",
-		tags: ["Blog"],
-		weight: 9,
-		enabled: false, 
-	},
-	{
 		title: "AcoFork Blog",
 		imgurl: "https://q2.qlogo.cn/headimg_dl?dst_uin=2726730791&spec=0",
 		desc: "Protect What You Love.",
 		siteurl: "https://acofork.com",
+		tags: ["Blog"],
+		weight: 9,
+		enabled: true,
+	},
+	{title: "kiyukie'Blog",
+		imgurl: "https://cdn.nodeimage.com/i/Q6NDWWd1h3I18zBNTwxFJABm5iKuGkuW.webp",
+		desc: "剑气纵横三万里，一剑光寒十九洲。",
+		siteurl: "https://blog.030666.xyz",
 		tags: ["Blog"],
 		weight: 9,
 		enabled: true,
@@ -53,6 +52,15 @@ export const friendsConfig: FriendLink[] = [
 		tags: ["Theme"],
 		weight: 8,
 		enabled: true,
+	},
+	{
+		title: "Firefly Docs",
+		imgurl: "https://docs-firefly.cuteleaf.cn/logo.png",
+		desc: "Firefly主题模板文档",
+		siteurl: "https://docs-firefly.cuteleaf.cn",
+		tags: ["Docs"],
+		weight: 8,
+		enabled: false, 
 	},
 ];
 
